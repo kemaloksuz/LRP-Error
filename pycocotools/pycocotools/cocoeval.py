39a11646b1400f9f723c36caa45f431b9b40e821
Kemal <kemal.oksuz@metu.edu.tr>
2020-06-29 00:00:15 +0300

    Format fix

--- pycocotools/pycocotools/cocoeval.py
+++ pycocotools/pycocotools/cocoeval.py
@@ -539,8 +539,7 @@ class COCOeval:
                 s = s[:, :, aind, mind]
             else:
                 # # dimension of LRP: [TxSxKxAxM]
-                if iouThr is not None:
-                    t = np.where(iouThr == p.iouThrs)[0]
+                t = np.where(iouThr == p.iouThrs)[0]
                 if lrp_type == 'aLRP':
                     s = self.eval['lrp'][t, :, :, aind, mind]
                     titleStr = 'Average LRP'
