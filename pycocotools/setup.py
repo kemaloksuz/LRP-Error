7b041836626b247ee545036c4317a47c9ef987a5
Kemal <kemal.oksuz@metu.edu.tr>
2020-11-18 16:43:50 +0200

    yapf format

--- pycocotools/setup.py
+++ pycocotools/setup.py
@@ -14,15 +14,11 @@ ext_modules = [
     )
 ]
 
-setup(
-    name='pycocotools',
-    packages=['pycocotools'],
-    package_dir={'pycocotools': 'pycocotools'},
-    install_requires=[
-        'setuptools>=18.0',
-        'cython>=0.27.3',
-        'matplotlib>=2.1.0'
-    ],
-    version='2.0',
-    ext_modules=ext_modules
-)
+setup(name='pycocotools',
+      packages=['pycocotools'],
+      package_dir={'pycocotools': 'pycocotools'},
+      install_requires=[
+          'setuptools>=18.0', 'cython>=0.27.3', 'matplotlib>=2.1.0'
+      ],
+      version='2.0',
+      ext_modules=ext_modules)
