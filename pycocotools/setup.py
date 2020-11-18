055eec31c172c3d0ac74e4f7b851c8095aabb644
Kemal <kemal.oksuz@metu.edu.tr>
2020-11-18 16:59:30 +0200

    setup corrected

--- pycocotools/setup.py
+++ pycocotools/setup.py
@@ -5,12 +5,17 @@ from setuptools import Extension, setup
 # To install library to Python site-packages run "python setup.py
 # build_ext install"
 
+# Note that the original compile flags below are GCC flags unsupported by
+# the Visual C++ 2015 build tools.
+# They can safely be removed.
+
 ext_modules = [
     Extension(
         'pycocotools._mask',
         sources=['common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs=[np.get_include(), '../common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        include_dirs=[np.get_include(), 'common'],
+        # extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        extra_compile_args=[],
     )
 ]
 
