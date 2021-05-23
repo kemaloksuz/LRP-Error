ef4b7baee7fb4f5cf009ecaa7cf7ec2acb1a9aba
Kemal <kemal.oksuz@metu.edu.tr>
2021-05-23 12:03:36 +0300

    lvis demo updated

--- lvis-api/demo.py
+++ lvis-api/demo.py
@@ -1,14 +1,27 @@
-from lvis import LVISEval
+from lvis import LVIS, LVISResults, LVISEval
 
 # result and val files for 100 randomly sampled images.
 ANNOTATION_PATH = "./data/lvis_val_100.json"
 RESULT_PATH = "./data/lvis_results_100.json"
 
-ANN_TYPE = 'bbox'
+# Annotation type is segm or bbox
+ANN_TYPE = 'segm'
 
-lvis_eval = LVISEval(ANNOTATION_PATH, RESULT_PATH, ANN_TYPE)
+# Number of detections to be collected from each image
+# LVIS uses 300 by default. 
+# If you want to use all detections in the detection 
+# file, then you can set it to -1.
+MAX_DETS = 300
+
+gt = LVIS(ANNOTATION_PATH)
+results = LVISResults(gt, RESULT_PATH, max_dets=MAX_DETS)
+lvis_eval = LVISEval(gt, results, iou_type=ANN_TYPE)
+params = lvis_eval.params
+params.max_dets = MAX_DETS  # No limit on detections per image.
+
 lvis_eval.run()
 lvis_eval.print_results()
+
 
 # Uncomment to print class-specific LRP-Optimal Thresholds
 # lvis_eval.print_lrp_opt_thresholds()
